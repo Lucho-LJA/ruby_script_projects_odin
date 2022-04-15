d161cdf2849c600a6c9cfaab387be50b8983f6be
Lucho-LJA <ljallauca.a@gmail.com>
2022-04-14 20:31:38 -0500

    Add inputData method in module of mode 1

--- mastermind.rb
+++ mastermind.rb
@@ -68,8 +68,45 @@ module Game_steps
         puts "(3) Exit"
         return gets.chomp
     end
+    def Game_steps.inputData(array)
+        arrayColor = []
+        if array.length == 4
+            array.each do |color|
+                case color
+                when "r"
+                    arrayColor.push("RED")
+                when "g"
+                    arrayColor.push("GREEN")
+                when "b"
+                    arrayColor.push("BLUE")
+                when "y"
+                    arrayColor.push("YELLOW")
+                else
+                    return []
+                end
+            end
+        else
+            return []
+        end
+        return arrayColor
+    end
 end
 
 #Program principal
+
+end_game = false
+option_mode = 0
 player=Player.new(Game_steps.title)
-Game_steps.mode_game(player.get_name)
+while option_mode < 1 or option_mode > 3 do
+    option_mode = Game_steps.mode_game(player.get_name).to_i
+end
+if option_mode == 1 
+    cont_games = 12
+    Game.generate_pass
+    while cont_games > 0 do
+        puts "Enter the colors's code r-red, g-green, b-blue, y-yellow (Example: rgby)"
+        p Game_steps.inputData(gets.chomp.split(''))
+        cont_games = 0
+
+    end
+end
